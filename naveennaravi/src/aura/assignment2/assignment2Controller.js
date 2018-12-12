1808fa0c43e3eb3bf2238883aada0b18943debc3
Naveen <poojari.n@ivymobility.com>
2018-12-12 15:02:29 +0530

    My first commit to git

---
+++ naveennaravi/src/aura/assignment2/assignment2Controller.js
@@ -0,0 +1,10 @@
+({
+	handleClick : function(component, event, helper) {
+        var x=component.get("v.val");
+        var y =component.find('input').get('v.value');
+        alert(x);
+		alert(y);
+   
+
+	}
+})
